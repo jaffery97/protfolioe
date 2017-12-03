5995ab6ba80e2caadad1d098a4f130022ed31008
jaffery97 <hasanjafory@gmail.com>
2017-12-03 02:19:03 -0500

    homead

--- contact.php
+++ contact.php
@@ -78,7 +78,7 @@ $headers = 'From: '.$email_from."\r\n".
 'Reply-To: '.$email_from."\r\n" .
 'X-Mailer: PHP/' . phpversion();
 @mail($email_to, $email_subject, $email_message, $headers);  
-    header('location:https://www.facebook.com/');
+    header('location:../contact1.html');
 ?>
  
 <!-- include your own success html here -->
